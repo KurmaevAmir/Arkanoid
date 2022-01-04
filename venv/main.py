4d6c179f95631a7b69a4aad3367b83dfcdcbeb1a
KurmaevAmir <amirku05@yandex.ru>
2022-01-04 14:18:28 +0300

    Modification of the "load_image" function

--- venv/main.py
+++ venv/main.py
@@ -10,6 +10,13 @@ def load_image(name, colorkey=None):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
     return image
 
 
