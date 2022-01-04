6681ee50dab91e6ca665d9580d6ece700f8623a9
KurmaevAmir <amirku05@yandex.ru>
2022-01-04 16:51:25 +0300

    Physics

---
+++ venv/Physics.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import random
+
+import pygame
+
+
+def load_image(name):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+FPS = 60
+WIDTH, HEIGHT = 600, 600
+
+all_sprites = pygame.sprite.Group()
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+
+
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    pygame.init()
+    clock = pygame.time.Clock()
+    fon = pygame.transform.scale(load_image('fon.jpg'),
+                                 (WIDTH, HEIGHT))
+    size = WIDTH, HEIGHT
+    screen = pygame.display.set_mode(size)
+    screen.blit(fon, (0, 0))
+
+    border = Border(5, 5, WIDTH - 5, 5)
+    border1 = Border(5, HEIGHT - 5, WIDTH - 5, HEIGHT - 5)
+    border2 = Border(5, 5, 5, HEIGHT - 5)
+    border3 = Border(WIDTH - 5, 5, WIDTH - 5, HEIGHT - 5)
+    ball = Ball(10, 295, 443)
+    horizontal_borders.add(border, border1)
+    vertical_borders.add(border2, border3)
+    all_sprites.add(border, border1, border2, border3)
+    all_sprites.add(ball)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        all_sprites.update()
+        screen.blit(fon, (0, 0))
+        all_sprites.draw(screen)
+        pygame.display.update()
+        clock.tick(FPS)
