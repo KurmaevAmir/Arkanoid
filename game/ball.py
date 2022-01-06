14a052760e109bed003f7828d8be885dd6d5679e
1mpr0ve <adelsalyamov@gmail.com>
2022-01-06 11:56:06 +0300

    Исправлен баг с начальным положением мяча

--- game/ball.py
+++ game/ball.py
@@ -7,7 +7,7 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.x_pos = Constants.screen_width / 2
-        self.y_pos = Constants.screen_height - 42
+        self.y_pos = Constants.screen_height - 66
         self.image = pygame.image.load('assets/my_ball.png').convert_alpha()
         self.rect = self.image.get_rect()
         self.velocity = [randint(2, 4), randint(-4, 0)]
@@ -30,7 +30,7 @@ class Ball(pygame.sprite.Sprite):
     def reset(self):
         self.velocity = [randint(2, 4), randint(-4, 0)]
         self.x_pos = Constants.screen_width / 2
-        self.y_pos = Constants.screen_height - 42
+        self.y_pos = Constants.screen_height - 66
         self.rect.center = (self.x_pos, self.y_pos)
 
     def check_collide_paddle(self, paddle):
